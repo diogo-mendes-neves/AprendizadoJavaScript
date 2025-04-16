5bafdb59648876eaeae1e2b02b65eef5aac3bfe3
Diogo Mendes <diogomendesnevesca@gmail.com>
2025-04-16 18:52:23 -0300

    Terceiro desafio - teste simples sobre a ligação html, js e css e uso de if/else

---
+++ TerceiroProjeto/index.js
@@ -0,0 +1,8 @@
+let productValue = Number(prompt('Enter de product value: '))
+
+if (productValue >= 20){
+    document.getElementById('result').innerHTML = 'Approved'
+}
+else{
+    document.getElementById('result').innerHTML = 'Denied'
+}
